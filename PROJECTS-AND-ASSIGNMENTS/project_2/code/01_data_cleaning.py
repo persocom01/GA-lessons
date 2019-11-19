ae45555dcd95f92b8bee340218fa126c08a77448
persocom01 <clarencett@hotmail.com>
2019-11-19 18:51:46 +0800

    0.60

--- PROJECTS-AND-ASSIGNMENTS/project_2/code/01_data_cleaning.py
+++ PROJECTS-AND-ASSIGNMENTS/project_2/code/01_data_cleaning.py
@@ -24,17 +24,33 @@ for k, v in df.isnull().sum().iteritems():
 print()
 
 # Since the false positive null values have already been screened at the data
-# loading stage, we now decide what to dow ith the remaining null values. It is
+# loading stage, we now decide what to do ith the remaining null values. It is
 # observed that garage_yr_blt is null for houses with no garage.
-print('invalid garage yr values:')
-print(df[(df['garage_type'] != 'NA') & (df['garage_yr_blt'].isnull())]
-      [['garage_type', 'garage_yr_blt']])
-print()
-# df = df.dropna()
 
-# Check for incorrect datatypes.
-for k, v in df.dtypes.iteritems():
-    print(k, v)
+# If lot_frontage is too weakly correlated to the saleprice, drop it.
+print(df[['lot_frontage', 'saleprice']].corr())
+# The correlation between lot_frontage and saleprice is not weak enough.
 
-# All datatypes confirmed to be correct.
-print(df.shape)
+# If lot_frontage is too strongly correlated to lot_area, drop it.
+print(df[['lot_frontage', 'lot_area']].corr())
+# The correlation between lot_frontage and saleprice is not strong enough.
+
+df2 = df
+df2['lot_frontage'] = df2['lot_frontage'].fillna(0)
+print(df2[['lot_frontage', 'saleprice']].corr())
+# Replacing null values in lot_frontage decreases the correlation between it
+# and the target. Thus I decided to drop the null rows.
+df = df.dropna(subset=['lot_frontage'])
+
+# print('invalid garage yr values:')
+# print(df[(df['garage_type'] != 'NA') & (df['garage_yr_blt'].isnull())]
+#       [['garage_type', 'garage_yr_blt']])
+# print()
+# # df = df.dropna()
+#
+# # Check for incorrect datatypes.
+# for k, v in df.dtypes.iteritems():
+#     print(k, v)
+# # All datatypes confirmed to be correct.
+#
+# print(df.shape)
