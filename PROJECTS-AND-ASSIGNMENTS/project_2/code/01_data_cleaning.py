01747960d71c43bdf09c90faf5ec3384441fadeb
persocom01 <clarencett@hotmail.com>
2019-11-19 09:14:32 +0800

    0.58

---
+++ PROJECTS-AND-ASSIGNMENTS/project_2/code/01_data_cleaning.py
@@ -0,0 +1,12 @@
+from python_imports import *
+import os
+
+print(os.getcwd())
+# Change working directory.
+# To go back up, use os.chdir('..').
+os.chdir(os.getcwd() + r'\files')
+
+import_path = r'.\PROJECTS-AND-ASSIGNMENTS\project_2\datasets\test.csv'
+# data = pd.read_csv(import_path)
+# df = pd.DataFrame(data)
+# print(df.head())
--- Project1234/project_2/code/01_data_cleaning.py
+++
@@ -1,6 +0,0 @@
-from python_imports import *
-
-import_path = r'.\datasets\test.csv'
-data = pd.read_csv(import_path)
-df = pd.DataFrame(data)
-print(df.head())
