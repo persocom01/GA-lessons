3ce39358fb0884ce826b840d9e1fe595f4f21e12
persocom01 <clarencett@hotmail.com>
2019-12-04 17:46:58 +0800

    0.73

--- PROJECTS-AND-ASSIGNMENTS/project_3/code/diagnostics.py
+++ PROJECTS-AND-ASSIGNMENTS/project_3/code/diagnostics.py
@@ -3,27 +3,28 @@
 
 class Sebastian:
 
-    def __init__(self, df=None, inplace=False):
-        if df is not None:
-            if not inplace:
-                self.df = df.copy()
-            else:
-                self.df = df
-        self.df_size = None
+    def __init__(self):
         self.null_dict = None
 
-    def get_nulls(self):
-        self.df_size = self.df.shape[0]
+    def get_nulls(self, df):
         self.null_dict = {}
-        for k, v in self.df.isnull().sum().iteritems():
-            self.null_dict[k] = v
+        for k, v in df.isnull().sum().iteritems():
+            if v > 0:
+                self.null_dict[k] = v
         return self.null_dict
 
     # Drops columns with 75% or more null values.
-    def drop_null_cols(self, null_size=0.75):
+    def drop_null_cols(self, df, null_size=0.75, inplace=False):
         if self.null_dict is None:
             raise Exception(r'use get_nulls(df) first.')
+        if inplace is False:
+            df = df.copy()
+        df_size = df.shape[0]
         for k, v in self.null_dict.items():
-            if v/self.df_size >= null_size:
-                del self.df[k]
-        return self.df
+            if null_size <= 1:
+                if v/df_size >= null_size:
+                    del df[k]
+            else:
+                if v >= null_size:
+                    del df[k]
+        return df
